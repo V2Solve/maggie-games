82d5cd2a022ac9be9bb26d0f06a45d67d41d527a
rishimagiawala <rishimagiawala@gmail.com>
2022-02-26 15:00:35 -0500

    user created enum

--- framework/modules/business-logic/constants.ts
+++ framework/modules/business-logic/constants.ts
@@ -2,6 +2,7 @@ export enum ValidationMessages
 {
     USER_ALREADY_EXIST = "User Already Exists",
     hello = 'hello',
-    INVALID_DATA = "The data passed is Invalid"
+    INVALID_DATA = "The data passed is Invalid",
+    USER_CREATED = "User was created"
 
 }
